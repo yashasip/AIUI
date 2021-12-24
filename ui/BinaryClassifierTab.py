2a99acb518917f9472e9bb847fd65ffdfac8049c
Yashas Ip <yashasip7@gmail.com>
2021-12-24 10:42:14 +0530

    Structured Code of ui/BinaryClassifierTab.py

--- ui/BinaryClassifierTab.py
+++ ui/BinaryClassifierTab.py
@@ -8,58 +8,60 @@ from ui.DataTable import DataTable
 class BinaryClassifierTab:
     def __init__(self) -> None:
         self.tab = QtWidgets.QWidget()
-
         self.tabLayout = QtWidgets.QWidget(self.tab)
+
+        self.binaryInnerTabLayout = QtWidgets.QVBoxLayout(self.tabLayout)
+        self.binaryTabFrame = QtWidgets.QFrame(self.tabLayout)
+
+        # Config components
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.binaryTabFrame)
+        self.ConfigLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        
+        # Table & related Components
+        self.tableLayout = QtWidgets.QWidget(self.binaryTabFrame)
+        self.inputTableLayout = QtWidgets.QVBoxLayout(self.tableLayout)
+        self.tableHorizontalButtonsLayout = QtWidgets.QHBoxLayout(self.tableLayout)
+        self.predictBtn = QtWidgets.QPushButton("Predict",self.tableLayout)
+        self.saveBtn = QtWidgets.QPushButton("Save",self.tableLayout)
+        self.tableButtonSpacer1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.tableButtonSpacer2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        
+        # User Defined Components
+        self.FileHandler = FileHandle(self.binaryTabFrame)
+        self.Config = ConfigGroup(parent=self.horizontalLayoutWidget)
+        self.inputTable = DataTable(self.tableLayout)
+
+        self.setupBinaryClassifierTab()
+
+    def setupBinaryClassifierTab(self):
         self.tabLayout.setContentsMargins(0, 0, 0, 0)
         self.tabLayout.setGeometry(QtCore.QRect(19, 9, 1199, 651))
-        self.binaryInnerTabLayout = QtWidgets.QVBoxLayout(self.tabLayout)
         self.binaryInnerTabLayout.setContentsMargins(0, 0, 0, 0)
 
-        self.binaryTabFrame = QtWidgets.QFrame(self.tabLayout)
         self.binaryTabFrame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.binaryTabFrame.setFrameShadow(QtWidgets.QFrame.Raised)
 
-        # File, File Input Handler
-        self.FileHandler = FileHandle(self.binaryTabFrame)
-
         # Config box layout
-        self.horizontalLayoutWidget = QtWidgets.QWidget(self.binaryTabFrame)
         self.horizontalLayoutWidget.setGeometry(QtCore.QRect(5, 78, 581, 561))
         self.horizontalLayoutWidget.setContentsMargins(0, 0, 0, 0)
 
-        self.ConfigLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
         self.ConfigLayout.setContentsMargins(0, 0, 0, 0)
 
         # Config Box
-        self.Config = ConfigGroup(parent=self.horizontalLayoutWidget)
         self.ConfigLayout.addWidget(self.Config.ConfigGroupBox)
 
         # Table Layout
-        self.tableLayout = QtWidgets.QWidget(self.binaryTabFrame)
         self.tableLayout.setGeometry(QtCore.QRect(609, 99, 580, 540))
 
-        self.inputTableLayout = QtWidgets.QVBoxLayout(self.tableLayout)
         self.inputTableLayout.setContentsMargins(0, 0, 0, 0)
-        
-        self.tableHorizontalButtonsLayout = QtWidgets.QHBoxLayout(self.tableLayout)
-
-        self.tableButtonSpacer1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.tableButtonSpacer2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-
 
         self.tableHorizontalButtonsLayout.addItem(self.tableButtonSpacer1)
-
-        self.predictBtn = QtWidgets.QPushButton("Predict",self.tableLayout)
         self.tableHorizontalButtonsLayout.addWidget(self.predictBtn)
 
-        self.saveBtn = QtWidgets.QPushButton("Save",self.tableLayout)
         self.tableHorizontalButtonsLayout.addWidget(self.saveBtn)
-
-
         self.tableHorizontalButtonsLayout.addItem(self.tableButtonSpacer2)
 
-        # input table set
-        self.inputTable = DataTable(self.tableLayout)
+        # input table & related components set
         self.inputTableLayout.addWidget(self.inputTable.inputDataTable)
         self.inputTableLayout.addLayout(self.tableHorizontalButtonsLayout)
         self.binaryInnerTabLayout.addWidget(self.binaryTabFrame)
