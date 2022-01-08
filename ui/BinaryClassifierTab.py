3e7d515c4e0a80f4078a53040374d07216b78dd9
Yashas Ip <yashasip7@gmail.com>
2022-01-09 00:11:26 +0530

    Added trainingLabel event and functionality

--- ui/BinaryClassifierTab.py
+++ ui/BinaryClassifierTab.py
@@ -85,6 +85,7 @@ class BinaryClassifierTab:
 
     
     def trainModel(self):
+        self.config.modelConfig.trainingLabel.setHidden(False)
         self.predictor = DataPredictor(
             dataRecord= self.chosenFile,
             selectedHeaders = list(self.config.headersListBox.selectedItems()),
@@ -93,6 +94,7 @@ class BinaryClassifierTab:
             activationFunction = self.config.modelConfig.activationFunctionComboBox.currentText().lower(),
             optimizerType = self.config.modelConfig.optimizerComboBox.currentText()) # *** scaling not set
 
+        self.config.modelConfig.trainingLabel.setHidden(True)
         self.predictor.trainModel()
 
     def prediction(self):
