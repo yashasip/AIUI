4fbb5b991cfb651c661e9619eea1ad0305eac3da
Yashas Ip <yashasip7@gmail.com>
2022-01-11 21:59:04 +0530

    Added openDirectory() which opens directory

--- handle/FileHandle.py
+++ handle/FileHandle.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import os
 
+
 class FileHandle:
     def __init__(self, parent, fileFilter) -> None:
         self.fileFilter = fileFilter
@@ -9,23 +10,29 @@ class FileHandle:
 
     def chooseFile(self):
         fileObj = QtWidgets.QFileDialog.getOpenFileName(
-            parent=self.parentWidget,
-            caption = 'Choose File',
-            filter = self.fileFilter
+            parent=self.parentWidget, caption="Choose File", filter=self.fileFilter
         )
-        if fileObj[0]: # if no file is chosen / Cancel button is clicked
+        if fileObj[0]:  # if no file is chosen / Cancel button is clicked
             self.filePath = fileObj[0]
-        
+
         return self.filePath
 
     def viewFile(self):
-        os.startfile(self.filePath) # should show a dialog box in case of an error
+        os.startfile(self.filePath)  # should show a dialog box in case of an error
 
     def saveFile(self, fileType):
         name = QtWidgets.QFileDialog.getSaveFileName(
-            parent = self.parentWidget,
-            caption = 'Save File',
-            filter = fileType,
-            initialFilter = fileType
+            parent=self.parentWidget,
+            caption="Save File",
+            filter=fileType,
+            initialFilter=fileType,
         )
         return name
+
+    def openDirectory(self):
+        dir = QtWidgets.QFileDialog.getExistingDirectory(
+            self.parentWidget,
+            caption  = "Open Directory containing the Model",
+            options = QtWidgets.QFileDialog.ShowDirsOnly | QtWidgets.QFileDialog.DontResolveSymlinks
+        )
+        return dir
